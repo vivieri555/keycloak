f236642cab88b04fe99cd2c9b7edc9336ce4e68e
unknown <vivieri555@gmail.com>
2026-03-26 14:50:12 +0100

    first

---
+++ src/test/java/com/Vivianne/KeycloakNew/KeycloakNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vivianne.KeycloakNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
